a304b8cb9473188447195b73b47ee4cf7cab5732
Akhil123-kor <74253091+Akhil123-kor@users.noreply.github.com>
2022-03-08 14:56:02 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,12 @@
+class ATM:  #To create class
+    def __init__(self, card, pin):  #this is the constructor in python
+        self.card=card
+        self.pin=pin
+
+    
+    def CashWithdrawl(self):
+        print("CashWithdrawl")
+
+    def BalanceEnquiry(self):
+        print("BalanceEnquiry")
+
